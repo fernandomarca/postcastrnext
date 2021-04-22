9b385e471e9b80fc468f038a871739f6906e56a7
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-04-21 21:16:39 -0300

    ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <div></div>;
+//SPA
+//SSR
+//SSG
+
+import { useEffect } from "react";
+
+export default function Home(props) {
+  useEffect(() => {}, []);
+  return (
+    <div>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, //8 hours
+  };
 }
